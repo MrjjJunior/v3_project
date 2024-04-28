0a0be83296f24ca9c8947a568b30ef34e0357222
Tshepiso <tlhongtshepiso2@gmail.com>
2024-04-28 16:19:33 +0200

    nja

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,5 +5,14 @@ from flask import jsonify
 # Route definition
 @app_views.route('/status', methods=['GET'])
 def status():
+    object_count = {
+
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+    }
     return jsonify({"status": "OK"})
 
